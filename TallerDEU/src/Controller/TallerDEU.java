1f321ff66d77c44aa33432a21d0206059fbe8c8b
IamBr4vo <bravo9386@gmail.com>
2023-09-21 16:27:21 -0600

    Taller1

---
+++ TallerDEU/src/Controller/TallerDEU.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Controller;
+
+import View.Ventana;
+
+/**
+ *
+ * @author Bravo
+ */
+public class TallerDEU {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ventana mostrar = new Ventana();
+        mostrar.setVisible(true);
+        mostrar.setResizable(false);
+        mostrar.setLocationRelativeTo(null);
+    }
+    
+}
